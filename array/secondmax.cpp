f53f2ac47fe78439b27a9ee6e26e54e1958c4a21
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-01 21:40:57 +0530

    Update secondmax.cpp

--- array/secondmax.cpp
+++ array/secondmax.cpp
@@ -8,12 +8,12 @@ int main()
     {
         cin>>a[i];
     }
-    l=INT64_MIN;
+    l=INT32_MIN;
     for(int i=1;i<5;i++)
     {
         l=max(l,a[i]);
     }
-    sl=INT64_MIN;
+    sl=INT32_MIN;
     for(int i=0;i<5;i++)
     {
         if(a[i]!=l)
